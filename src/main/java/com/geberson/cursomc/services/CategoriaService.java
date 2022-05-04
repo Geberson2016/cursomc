2cb5b67f0175bcc31f15c038adfe2913800935ca
Geberson Antonio <geberson2016@gmail.com>
2022-05-04 16:04:16 -0300

    Criando exceptions para tratamento de objetos nao encontrados

--- src/main/java/com/geberson/cursomc/services/CategoriaService.java
+++ src/main/java/com/geberson/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.geberson.cursomc.services;
 
 import com.geberson.cursomc.domain.Categoria;
 import com.geberson.cursomc.repositories.CategoriaRepository;
+import com.geberson.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ public class CategoriaService {
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
